8dbb748f90acb315c3f8df2ae6a196147ef64c94
s6704062616036-collab <s6704062616036@email.kmutnb.ac.th>
2026-03-02 23:32:26 +0700

    Update lab9_3no4.c

--- lab9_3no4.c
+++ lab9_3no4.c
@@ -1,59 +1,31 @@
 #include <stdio.h>
 #define MAX 50
-
-int countVowel(char []);
-
+int countVowel(char t[], int *a, int *e, int *ii, int *o, int *u);
 void main() {
     char text[MAX];
     int cVowel;
-    
+    int ca, ce, ci, co, cu;
+
     printf("Enter text : ");
     scanf("%s", text);
-    
-    cVowel = countVowel(text); 
-    
+    cVowel = countVowel(text, &ca, &ce, &ci, &co, &cu);
     printf("Text : [%s] has %d vowels\n", text, cVowel);
+    printf("A/a = %d\n", ca);
+    printf("E/e = %d\n", ce);
+    printf("I/i = %d\n", ci);
+    printf("O/o = %d\n", co);
+    printf("U/u = %d\n", cu);
 }
-
-int countVowel(char t[]) {
-    int i = 0, count = 0;
-    
-   
-    int cA = 0, cE = 0, cI = 0, cO = 0, cU = 0; 
-
-    while (i < MAX && t[i] != '\0') {
-        
-        if (t[i] == 'A' || t[i] == 'a') {
-            cA++;
-            count++;
-        } 
-        else if (t[i] == 'E' || t[i] == 'e') {
-            cE++;
-            count++;
-        } 
-        else if (t[i] == 'I' || t[i] == 'i') {
-            cI++;
-            count++;
-        } 
-        else if (t[i] == 'O' || t[i] == 'o') {
-            cO++;
-            count++;
-        } 
-        else if (t[i] == 'U' || t[i] == 'u') {
-            cU++;
-            count++;
-        }
+int countVowel(char t[], int *a, int *e, int *ii, int *o, int *u){
+    int i = 0, cVowel = 0;
+    *a = *e = *ii = *o = *u = 0;
+    while(i < MAX && t[i] != '\0') {
+        if (t[i]=='A'||t[i]=='a'){(*a)++; cVowel++;}
+        if (t[i]=='E'||t[i]=='e'){(*e)++; cVowel++;}
+        if (t[i]=='I'||t[i]=='i'){(*ii)++; cVowel++;}
+        if (t[i]=='O'||t[i]=='o'){(*o)++; cVowel++;}
+        if (t[i]=='U'||t[i]=='u'){(*u)++; cVowel++;}
         i++;
     }
-
-   
-    printf("--- Vowel Details ---\n");
-    printf("A = %d\n", cA);
-    printf("E = %d\n", cE);
-    printf("I = %d\n", cI);
-    printf("O = %d\n", cO);
-    printf("U = %d\n", cU);
-    
-
-    return(count);
+    return cVowel;
 }
