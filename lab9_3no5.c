e77a1ce33235cad849a886005aa74bdad21c1d19
s6704062616036-collab <s6704062616036@email.kmutnb.ac.th>
2026-03-02 23:31:46 +0700

    Refactor main function and sumDigitChar logic

--- lab9_3no5.c
+++ lab9_3no5.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
-int main() {
-    char text[100];
-    int sum = 0;
-    int i = 0;
-
+#define MAX 200
+int sumDigitChar(char s[]);
+void main() {
+    char text[MAX];
     printf("Enter input: ");
-    scanf("%[^\n]", text);
+    gets(text);
 
-    while (text[i] != '\0') {
-        if (text[i] >= '0' && text[i] <= '9') {
-            sum += (text[i] - '0');
-        }
+    printf("Sum of digit char: %d\n", sumDigitChar(text));
+}
+int sumDigitChar(char s[]) {
+    int i = 0, sum = 0;
+    while (i < MAX && s[i] != '\0') {
+        if (s[i] >= '0' && s[i] <= '9')
+            sum += (s[i] - '0');
         i++;
     }
-
-    printf("Sum of digit char: %d\n", sum);
-
-    return 0;
+    return sum;
 }
